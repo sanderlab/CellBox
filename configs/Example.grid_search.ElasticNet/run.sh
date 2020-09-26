a191b77b0d9087c1ac878972e95b02b6a84edf3a
Bo Yuan <boyuan@g.harvard.edu>
2020-09-25 17:36:14 -0700

    Clean up configs folder

--- configs/Example.grid_search.ElasticNet/run.sh
+++ configs/Example.grid_search.ElasticNet/run.sh
@@ -13,7 +13,7 @@ module load python/3.7.4
 source $labhome/bo/venv37/bin/activate
 cd $labhome/bo/CellBox/CellBox
 
-for i in {200..299}
+for i in {000..099}
     do
         python scripts/main.py -config="${1}" -i=$i
     done

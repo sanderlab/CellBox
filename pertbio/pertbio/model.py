b8d6ba5160fc5a7b760066c17251f77389fda803
Bo Yuan <boyuan@g.harvard.edu>
2020-09-25 17:40:49 -0700

    Move CellBox model to the top of model.py

--- pertbio/pertbio/model.py
+++ pertbio/pertbio/model.py
@@ -82,6 +82,121 @@ class PertBio:
         return self
 
 
+class CellBox(PertBio):
+    """CellBox model"""
+    def build(self):
+        self.params = {}
+        self.get_variables()
+        if self.args.pert_form == 'by u':
+            y0 = tf.constant(np.zeros((self.n_x, 1)), name="x_init", dtype=tf.float32)
+            self.train_y0 = y0
+            self.monitor_y0 = y0
+            self.eval_y0 = y0
+            self.gradient_zero_from = None
+        elif self.args.pert_form == 'fix x':  # fix level of node x (here y) by input perturbation u (here x)
+            self.train_y0 = tf.transpose(self.train_x)
+            self.monitor_y0 = tf.transpose(self.monitor_x)
+            self.eval_y0 = tf.transpose(self.eval_x)
+            self.gradient_zero_from = self.args.n_activity_nodes
+        self.envelope_fn = pertbio.kernel.get_envelope(self.args)
+        self.ode_solver = pertbio.kernel.get_ode_solver(self.args)
+        self._dxdt = pertbio.kernel.get_dxdt(self.args, self.params)
+        self.convergence_metric_train, self.train_yhat = self.forward(self.train_y0, self.train_x)
+        self.convergence_metric_monitor, self.monitor_yhat = self.forward(self.monitor_y0, self.monitor_x)
+        self.convergence_metric_eval, self.eval_yhat = self.forward(self.eval_y0, self.eval_x)
+        self.get_ops()
+        return self
+
+    def forward(self, y0, mu):
+        if isinstance(mu, tf.SparseTensor):
+            mu_t = tf.sparse.to_dense(tf.sparse.transpose(mu))
+        else:
+            mu_t = tf.transpose(mu)
+        ys = self.ode_solver(y0, mu_t, self.args.dT, self.args.n_T, self._dxdt, self.gradient_zero_from)
+        # [n_T, n_x, batch_size]
+        ys = ys[-self.args.ode_last_steps:]
+        # [n_iter_tail, n_x, batch_size]
+        mean, sd = tf.nn.moments(ys, axes=0)
+        yhat = tf.transpose(ys[-1])
+        dxdt = self._dxdt(ys[-1], mu_t)
+        # [n_x, batch_size] for last ODE step
+        convergence_metric = tf.concat([mean, sd, dxdt], axis=0)
+        return convergence_metric, yhat
+
+    def get_variables(self):
+        """
+        Initialize parameters in the Hopfield equation
+
+        Mutates:
+            self.params(dict):{
+                W (tf.Variable): interaction matrix with constraints enforced, , shape: [n_x, n_x]
+                alpha (tf.Variable): alpha, shape: [n_x, 1]
+                eps (tf.Variable): eps, shape: [n_x, 1]
+            }
+        """
+        n_x, n_protein_nodes, n_activity_nodes = self.n_x, self.args.n_protein_nodes, self.args.n_activity_nodes
+        with tf.compat.v1.variable_scope("initialization", reuse=True):
+            """
+               Enforce constraints  (i: recipient)
+               no self regulation wii=0
+               ingoing wij for drug nodes (88th to 99th) = 0 [n_activity_nodes 87: ]
+                                w [87:99,_] = 0
+               outgoing wij for phenotypic nodes (83th to 87th) [n_protein_nodes 82 : n_activity_nodes 87]
+                                w [_, 82:87] = 0
+               ingoing wij for phenotypic nodes from drug ndoes (direct) [n_protein_nodes 82 : n_activity_nodes 87]
+                                w [82:87, 87:99] = 0
+            """
+            W = tf.Variable(np.random.normal(0.01, size=(n_x, n_x)), name="W", dtype=tf.float32)
+            W_mask = (1.0 - np.diag(np.ones([n_x])))
+            W_mask[n_activity_nodes:, :] = np.zeros([n_x - n_activity_nodes, n_x])
+            W_mask[:, n_protein_nodes:n_activity_nodes] = np.zeros([n_x, n_activity_nodes - n_protein_nodes])
+            W_mask[n_protein_nodes:n_activity_nodes, n_activity_nodes:] = np.zeros([n_activity_nodes - n_protein_nodes,
+                                                                                    n_x - n_activity_nodes])
+            self.params['W'] = W_mask * W
+
+            eps = tf.Variable(np.ones((n_x, 1)), name="eps", dtype=tf.float32)
+            alpha = tf.Variable(np.ones((n_x, 1)), name="alpha", dtype=tf.float32)
+            self.params['alpha'] = tf.nn.softplus(alpha)
+            self.params['eps'] = tf.nn.softplus(eps)
+
+            if self.args.envelope == 2:
+                psi = tf.Variable(np.ones((n_x, 1)), name="psi", dtype=tf.float32)
+                self.params['psi'] = tf.nn.softplus(psi)
+
+
+class LinReg(PertBio):
+    """linear regression model"""
+    def get_variables(self):
+        with tf.compat.v1.variable_scope("initialization", reuse=True):
+            self.params.update({
+                'W': tf.Variable(np.random.normal(0.01, size=(self.n_x, self.n_x)), name="W", dtype=tf.float32),
+                'b': tf.Variable(np.random.normal(0.01, size=(self.n_x, 1)), name="b", dtype=tf.float32)
+            })
+
+    def forward(self, mu):
+        xhat = tf.matmul(mu, self.params['W']) + tf.reshape(self.params['b'], [1, -1])
+        return xhat
+
+
+class NN(LinReg):
+    """Neural network model"""
+    def get_variables(self):
+        with tf.compat.v1.variable_scope("initialization", reuse=True):
+            self.params.update({
+                'W_h': tf.Variable(np.random.normal(0.01, size=(self.n_x, self.args.n_hidden)), name="Wh",
+                                   dtype=tf.float32),
+                'b_h': tf.Variable(np.random.normal(0.01, size=(self.args.n_hidden, 1)), name="bh", dtype=tf.float32),
+                'W': tf.Variable(np.random.normal(0.01, size=(self.args.n_hidden, self.n_x)), name="Wo",
+                                 dtype=tf.float32),
+                'b': tf.Variable(np.random.normal(0.01, size=(self.n_x, 1)), name="bo", dtype=tf.float32)
+            })
+
+    def forward(self, mu):
+        hidden = tf.tanh(tf.matmul(mu, self.params['W_h']) + tf.reshape(self.params['b_h'], [1, -1]))
+        xhat = tf.matmul(hidden, self.params['W']) + tf.reshape(self.params['b'], [1, -1])
+        return xhat
+
+
 def get_idx_pair(mu):
     """get perturbation position"""
     idx = np.where(mu != 0)[0]
@@ -178,118 +293,3 @@ class CoExpNonlinear(CoExp):
     # return xhat
 
 
-class LinReg(PertBio):
-    """linear regression model"""
-    def get_variables(self):
-        with tf.compat.v1.variable_scope("initialization", reuse=True):
-            self.params.update({
-                'W': tf.Variable(np.random.normal(0.01, size=(self.n_x, self.n_x)), name="W", dtype=tf.float32),
-                'b': tf.Variable(np.random.normal(0.01, size=(self.n_x, 1)), name="b", dtype=tf.float32)
-            })
-
-    def forward(self, mu):
-        xhat = tf.matmul(mu, self.params['W']) + tf.reshape(self.params['b'], [1, -1])
-        return xhat
-
-
-class NN(LinReg):
-    """Neural network model"""
-    def get_variables(self):
-        with tf.compat.v1.variable_scope("initialization", reuse=True):
-            self.params.update({
-                'W_h': tf.Variable(np.random.normal(0.01, size=(self.n_x, self.args.n_hidden)), name="Wh",
-                                   dtype=tf.float32),
-                'b_h': tf.Variable(np.random.normal(0.01, size=(self.args.n_hidden, 1)), name="bh", dtype=tf.float32),
-                'W': tf.Variable(np.random.normal(0.01, size=(self.args.n_hidden, self.n_x)), name="Wo",
-                                 dtype=tf.float32),
-                'b': tf.Variable(np.random.normal(0.01, size=(self.n_x, 1)), name="bo", dtype=tf.float32)
-            })
-
-    def forward(self, mu):
-        hidden = tf.tanh(tf.matmul(mu, self.params['W_h']) + tf.reshape(self.params['b_h'], [1, -1]))
-        xhat = tf.matmul(hidden, self.params['W']) + tf.reshape(self.params['b'], [1, -1])
-        return xhat
-
-
-class CellBox(PertBio):
-    """CellBox model"""
-    def build(self):
-        self.params = {}
-        self.get_variables()
-        if self.args.pert_form == 'by u':
-            y0 = tf.constant(np.zeros((self.n_x, 1)), name="x_init", dtype=tf.float32)
-            self.train_y0 = y0
-            self.monitor_y0 = y0
-            self.eval_y0 = y0
-            self.gradient_zero_from = None
-        elif self.args.pert_form == 'fix x':  # fix level of node x (here y) by input perturbation u (here x)
-            self.train_y0 = tf.transpose(self.train_x)
-            self.monitor_y0 = tf.transpose(self.monitor_x)
-            self.eval_y0 = tf.transpose(self.eval_x)
-            self.gradient_zero_from = self.args.n_activity_nodes
-        else:
-            raise Exception("Illegal pert_form in the config file. Choose from ['by u', 'fix x'].")
-        self.envelop_fn = pertbio.kernel.get_envelop(self.args)
-        self.ode_solver = pertbio.kernel.get_ode_solver(self.args)
-        self._dxdt = pertbio.kernel.get_dxdt(self.args, self.params)
-        self.convergence_metric_train, self.train_yhat = self.forward(self.train_y0, self.train_x)
-        self.convergence_metric_monitor, self.monitor_yhat = self.forward(self.monitor_y0, self.monitor_x)
-        self.convergence_metric_eval, self.eval_yhat = self.forward(self.eval_y0, self.eval_x)
-        self.get_ops()
-        return self
-
-    def forward(self, x0, mu):
-        if isinstance(mu, tf.SparseTensor):
-            mu_t = tf.sparse.to_dense(tf.sparse.transpose(mu))
-        else:
-            mu_t = tf.transpose(mu)
-        xs = self.ode_solver(x0, mu_t, self.args.dT, self.args.n_T, self._dxdt, self.gradient_zero_from)
-        # [n_T, n_x, batch_size]
-        xs = xs[-self.args.ode_last_steps:]
-        # [n_iter_tail, n_x, batch_size]
-        mean, sd = tf.nn.moments(xs, axes=0)
-        xhat = tf.transpose(xs[-1])
-        dxdt = self._dxdt(xs[-1], mu_t)
-        # [n_x, batch_size] for last ODE step
-        convergence_metric = tf.concat([mean, sd, dxdt], axis=0)
-        return convergence_metric, xhat
-
-    def get_variables(self):
-        """
-        Initialize parameters in the Hopfield equation
-
-        Returns:
-            self.params(dict):{
-                W (tf.Variable): interaction matrix with constraints enforced, , shape: [n_x, n_x]
-                alpha (tf.Variable): alpha, shape: [n_x, 1]
-                eps (tf.Variable): eps, shape: [n_x, 1]
-            }
-        """
-        n_x, n_protein_nodes, n_activity_nodes = self.n_x, self.args.n_protein_nodes, self.args.n_activity_nodes
-        with tf.compat.v1.variable_scope("initialization", reuse=True):
-            # TODO: check to see if it make sense to change tf.Variables to tf.get_variables
-            '''Enforce constraints  (i: recipient)
-               no self regulation wii=0
-               ingoing wij for drug nodes (88th to 99th) = 0 [n_activity_nodes 87: ]
-                                w [87:99,_] = 0
-               outgoing wij for phenotypic nodes (83th to 87th) [n_protein_nodes 82 : n_activity_nodes 87]
-                                w [_, 82:87] = 0
-               ingoing wij for phenotypic nodes from drug ndoes (direct) [n_protein_nodes 82 : n_activity_nodes 87]
-                                w [82:87, 87:99] = 0
-            '''
-            W = tf.Variable(np.random.normal(0.01, size=(n_x, n_x)), name="W", dtype=tf.float32)
-            W_mask = (1.0 - np.diag(np.ones([n_x])))
-            W_mask[n_activity_nodes:, :] = np.zeros([n_x - n_activity_nodes, n_x])
-            W_mask[:, n_protein_nodes:n_activity_nodes] = np.zeros([n_x, n_activity_nodes - n_protein_nodes])
-            W_mask[n_protein_nodes:n_activity_nodes, n_activity_nodes:] = np.zeros([n_activity_nodes - n_protein_nodes,
-                                                                                    n_x - n_activity_nodes])
-            self.params['W'] = W_mask * W
-
-            eps = tf.Variable(np.ones((n_x, 1)), name="eps", dtype=tf.float32)
-            alpha = tf.Variable(np.ones((n_x, 1)), name="alpha", dtype=tf.float32)
-            self.params['alpha'] = tf.nn.softplus(alpha)
-            self.params['eps'] = tf.nn.softplus(eps)
-
-            if self.args.envelop == 2:
-                psi = tf.Variable(np.ones((n_x, 1)), name="psi", dtype=tf.float32)
-                self.params['psi'] = tf.nn.softplus(psi)

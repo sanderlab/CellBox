1dd5f5469c495c1d2e9717d40bd904a04ae5f2b9
Bo Yuan <boyuan@g.harvard.edu>
2020-09-25 17:44:39 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import pytest
 import os
 import glob
 
+
 def test_model():
-    os.system('python scripts/main.py -config=configs/debugger.cellbox.json')
-    files = glob.glob('results/Debugging_*/b11_000/3_best.W*')
+    os.system('python scripts/main.py -config=configs/Example.minimal.json')
+    files = glob.glob('results/Debugging_*/seed_000/3_best.W*')
     assert len(files)==1
+
 
 if __name__ == '__main__':
 

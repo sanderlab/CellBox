a4b35b82b5d2ca9bb1842458b67a3fe8a3c101d4
Desmond Yuan <desmond.yuan@hotmail.com>
2020-09-25 20:22:25 -0400

    modification from O2 server

---
+++ configs/ElasticNet/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH -t 0-12:00                         # Runtime in D-HH:MM format
+#SBATCH -p short
+#SBATCH --mem=10g                         # Memory total in MB (for all cores)
+#SBATCH -o slurm_out/%j.out
+#SBATCH -e slurm_out/%j.err
+
+source ~/.bash_profile
+
+module load gcc/6.2.0
+module load python/3.7.4
+source $labhome/bo/venv37/bin/activate
+cd $labhome/bo/CellBox/CellBox
+
+for i in {200..299}
+    do
+        python scripts/main.py -config="${1}" -i=$i
+    done
+
